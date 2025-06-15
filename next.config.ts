246fe354c181965fedd47528aaf3aae2a4fb4295
omar1779 <josemyclans@gmail.com>
2025-06-15 01:51:17 -0500

    one day 1.2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tailwindcss.com",
+      },
+      // agrega aquí otros dominios si usas más imágenes externas
+    ],
+  },
 };
 
 export default nextConfig;
